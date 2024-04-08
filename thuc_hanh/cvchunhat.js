84ca3d26cf3fdb632184ee701911ea8176195339
baoky1999 <kinataru0123@gmail.com>
2024-04-08 17:17:22 +0700

    baitap4bai

--- thuc_hanh/cvchunhat.js
+++
@@ -1,6 +0,0 @@
-inputWidth = prompt("Enter the Width");
-inputHeight= prompt("Enter the Height");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
-let area = width * height;
-document.write("The area is:" + area);
---
+++ thuc_hanh/doidoc.js
@@ -0,0 +1,6 @@
+
+function chuyenDoi(){
+    let doC = + document.ElementById("c").value;
+    let doF = (9*doc /5) + 32;
+    document.ElementById("result").innerHTML="result: " + doF;
+}
